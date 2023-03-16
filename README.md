cdfce1af72adc28013206deb468f3d8abeeea82f
MangoSwirl <collin.camerono@gmail.com>
2023-03-15 18:02:03 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple Timer
+
+A simple, bare-bones timer designed mostly for use in OBS. A live version is available at [https://mangoswirl.github.io/simple-timer](https://mangoswirl.github.io/simple-timer).
+
+To use it, just put the number of milliseconds into the URL, like this 5-minute example:
+```
+https://mangoswirl.github.io/simple-timer/#600000
+```
---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  let timerDuration = parseInt(window.location.hash.substring(1));
+
+  const startTime = Date.now();
+
+  let remaining = Math.max((startTime + timerDuration) - Date.now(), 0)
+
+  console.log(remaining);
+
+  function update() {
+    requestAnimationFrame(() => {
+      remaining = Math.max((startTime + timerDuration) - Date.now(), 0);
+      
+      update()
+    })
+  }
+
+  update()
+</script>
+
+{Math.floor((remaining / 1000) / 60).toString() + ":" + Math.floor((remaining / 1000) % 60).toString().padStart(2, "0")}
